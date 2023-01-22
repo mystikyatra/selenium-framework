a803eb62c2210bf103f23619969d36ca2943df2c
mystikyatra <pawankharel24@gmail.com>
2023-01-22 14:27:06 +0545

    import file in LoginScript removed

--- src/test/java/login/LoginScript.java
+++ src/test/java/login/LoginScript.java
@@ -3,7 +3,6 @@ package login;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.v85.log.Log;
 import util.SystemProperties;
 import util.Utils;
 import static login.loginSelectors.*;
